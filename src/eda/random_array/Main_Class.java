5d61ed69c34b49008747ad33735941e344759f98
fxalban8 <francisco.alban@epn.edu.ec>
2020-06-08 12:37:09 -0500

    Verifying the range

--- src/eda/random_array/Main_Class.java
+++ src/eda/random_array/Main_Class.java
@@ -1,18 +1,37 @@
 package eda.random_array;
 
 import java.util.Random;
-
+import java.util.Scanner;
 public class Main_Class {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		Random rd = new Random(); // creating Random object
-	      int[] arr = new int[5];
-	      for (int i = 0; i < arr.length; i++) {
-	         arr[i] = rd.nextInt(); // storing random integers in an array
-	         System.out.println(arr[i]); // printing each array element
+	    Scanner sc = new Scanner(System.in);  
+		int[] arr = new int[5];
+	    int x;  
+		int min, max;
+		System.out.println("Cuál es el rango?");
+		min= sc.nextInt();
+		max=sc.nextInt();
+	    for (int i = 0; i < arr.length; i++) {
+	    	  x=rd.nextInt(); // storing random integers
+	    	  if(verifyingInterval(x,min,max)) {
+	    		  arr[i] =x;
+	    		  System.out.println(arr[i]); // printing each array element
+	    	  }else {
+	    		  i-=1;
+	    	  }
+	    	  
+	    }	    
+	         
 	      }
-	}
+	
+
+
+public static boolean verifyingInterval(int x, int min, int max) {
+	return min<=x && x<=max;
+}
 }
 
